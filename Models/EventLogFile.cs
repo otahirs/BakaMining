06aaf0fc6243ae492709b7c0e07b2b1b003110f9
Otakar Hirš <xhirs@fi.muni.cz>
2021-08-11 18:32:24 +0200

    feat: event log storage

---
+++ Models/EventLogFile.cs
@@ -0,0 +1,11 @@
+﻿using ProcessM.NET.Model;
+using ProcessM.NET.Model.DataAnalysis;
+
+namespace BakaMining.Models
+{
+    public class EventLogFile
+    {
+        public FileMetadata Metadata { get; set; }
+        public WorkflowLog EventLog { get; set; }
+    }
+}
--- Services/Storage.cs
+++ Services/Storage.cs
@@ -2,17 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using BakaMining.Models;
+using ProcessM.NET.Model;
 
 namespace BakaMining.Services
 {
     public class Storage
     {
         private Dictionary<string, PetriNetFile> _nets = new ();
+        private Dictionary<string, EventLogFile> _logs = new ();
         public void AddPetriNetFile(PetriNetFile file)
         {
-            if (_nets.ContainsKey(file.Name))
+            if (_nets.ContainsKey(file.Metadata.Name))
                 throw new ArgumentException();
-            _nets[file.Name] = file;
+            _nets[file.Metadata.Name] = file;
         }
 
         public void RemovePetriNetFile(string name)
@@ -20,14 +22,36 @@ namespace BakaMining.Services
             _nets.Remove(name);
         }
 
-        public List<PetriNetFile> GetAll()
+        public List<PetriNetFile> GetAllNets()
         {
             return _nets.Values.ToList();
         }
 
-        public PetriNetFile Get(string filename)
+        public PetriNetFile GetPetriNet(string filename)
         {
             return _nets[filename];
+        }
+        
+        //
+        public void AddEventLogFile(EventLogFile file)
+        {
+            if (_nets.ContainsKey(file.Metadata.Name))
+                throw new ArgumentException();
+            _logs[file.Metadata.Name] = file;
+        }
+        public void RemoveLogFile(string name)
+        {
+            _logs.Remove(name);
+        }
+
+        public List<EventLogFile> GetAllLogs()
+        {
+            return _logs.Values.ToList();
+        }
+
+        public EventLogFile GetEventLog(string filename)
+        {
+            return _logs[filename];
         }
     }
 }
