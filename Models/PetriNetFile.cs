4a363fcf0fb1aed2def507ad19f467892bb8ec48
Otakar Hirš <xhirs@fi.muni.cz>
2021-08-30 19:46:33 +0200

    feat: Store files permanently in IndexedDB

--- Models/PetriNetFile.cs
+++ Models/PetriNetFile.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using ProcessM.NET.Model;
+﻿using ProcessM.NET.Model.BasicPetriNet;
 
 namespace BakaMining.Models
 {
     public class PetriNetFile
     {
         public FileMetadata Metadata { get; set; }
-        public IPetriNet PetriNet { get; set; }
+        public PetriNet PetriNet { get; set; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,10 +1,18 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Net.Http;
+using System.Reflection;
 using System.Threading.Tasks;
-using BakaMining.Services;
+using BakaMining.Services.Storage;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using MudBlazor.Services;
+using TG.Blazor.IndexedDB;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Microsoft.JSInterop;
+using ProcessM.NET.Model;
+using ProcessM.NET.Model.BasicPetriNet;
 
 namespace BakaMining
 {
@@ -16,11 +24,65 @@ namespace BakaMining
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddScoped(sp => new Storage());
-
             builder.Services.AddMudServices();
+            
+            builder.Services.AddIndexedDB(dbStore =>
+            {
+                dbStore.DbName = "BakaStorage";
+                dbStore.Version = 1;
 
-            await builder.Build().RunAsync();
+                dbStore.Stores.Add(new StoreSchema
+                {
+                    Name = Enums.Storename.EventLogFile,
+                    PrimaryKey = new IndexSpec{Name="filename", KeyPath = "metadata.name", Auto=false},
+                    
+                });
+                dbStore.Stores.Add(new StoreSchema
+                {
+                    Name = Enums.Storename.PetriNetFile,
+                    PrimaryKey = new IndexSpec{Name="filename", KeyPath = "metadata.name", Auto=false},
+                });
+            });
+            builder.Services.AddScoped<EventLogStore>();
+            builder.Services.AddScoped<PetriNetStore>();
+            
+            /* Does NOT work, see {workaround}
+            To deserialize JSON object from Interfaces in JSInterop globally.
+            https://github.com/dotnet/aspnetcore/issues/12685
+            builder.Services.Configure<JsonSerializerOptions>(options =>
+            {
+                options.Converters.Add(new AbstractConverter<IPlace, Place>());
+                options.Converters.Add(new AbstractConverter<ITransition, Transition>());
+            });
+            */
+
+            var host = builder.Build();
+            ConfigureProviders(host.Services);
+            await host.RunAsync();
+            //await builder.Build().RunAsync();
         }
+
+        // Messy {workaround}
+        // To deserialize JSON object from Interfaces in JSInterop globally.
+        public static void ConfigureProviders(IServiceProvider services)
+        {
+            var jsRuntime = services.GetService<IJSRuntime>();
+            var prop = typeof(JSRuntime).GetProperty("JsonSerializerOptions",
+                BindingFlags.NonPublic | BindingFlags.Instance);
+            JsonSerializerOptions options = (JsonSerializerOptions)Convert.ChangeType(
+                prop.GetValue(jsRuntime, null), typeof(JsonSerializerOptions));
+            options.Converters.Add(new AbstractConverter<IPlace, Place>());
+            options.Converters.Add(new AbstractConverter<ITransition, Transition>());
+        }
+    }
+
+    public class AbstractConverter<TInterface, TImplementation> 
+        : JsonConverter<TInterface> where TImplementation : TInterface
+    {
+        [return: MaybeNull]
+        public override TInterface Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) 
+            => JsonSerializer.Deserialize<TImplementation>(ref reader, options);
+        public override void Write(Utf8JsonWriter writer, TInterface value, JsonSerializerOptions options) 
+            => JsonSerializer.Serialize(writer, (TImplementation)value!, options);
     }
 }
